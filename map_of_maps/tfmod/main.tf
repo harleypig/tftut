0867526e094621c34ab3179936a80c892fe3a149
Your Name <you@example.com>
2023-07-10 13:23:42 -0600

    added first part

---
+++ map_of_maps/tfmod/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    local = { source = "hashicorp/local" }
+  }
+}
+
+variable "files" {
+  type = map(object({
+    #filename    = string,
+    content     = string,
+    permissions = string
+  }))
+}
+
+#locals {
+#  files = { for file in var.files : file["filename"] => file }
+#}
+
+resource "local_file" "file" {
+  #for_each = local.files
+  for_each = var.files
+
+  filename        = each.key
+  content         = each.value["content"]
+  file_permission = each.value["permissions"]
+}
