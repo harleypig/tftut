9269213d9d055133f1b24eb8dc7959c1fc106d1a
Your Name <you@example.com>
2023-07-12 13:43:07 -0600

    fix formatting

--- loop_in_module/variables.tf
+++ loop_in_module/variables.tf
@@ -1,19 +1,19 @@
 variable "files" {
   type = list(object({
-    filename = string,
-    content = string,
+    filename    = string,
+    content     = string,
     permissions = string,
   }))
   description = "a list of files"
   default = [
     {
-      filename = "modloop1.txt"
-      content = "first file"
+      filename    = "modloop1.txt"
+      content     = "first file"
       permissions = "0644"
     },
     {
-      filename = "modloop2.txt"
-      content = "second file"
+      filename    = "modloop2.txt"
+      content     = "second file"
       permissions = "0755"
     }
   ]
