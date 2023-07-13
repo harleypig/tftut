544b264dc90fe32418f3d09fd5b353d57c1bcff8
Your Name <you@example.com>
2023-07-13 11:14:33 -0600

    aider: Added main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+locals {
+  _files = [
+    for file in fileset(var.yaml_dir, "**/*.yml") : [
+      for key, data in yamldecode(file("${var.yaml_dir}/${file}")) :
+      merge(data, {
+        filename    = "${key}"
+        content     = data.content
+        permissions = data.permissions
+  })]]
+
+  files = { for file in local._files : file["filename"] => file }
+}
+
+module "my_file" {
+  source = "./tfmod"
+  files  = local.files
+}
